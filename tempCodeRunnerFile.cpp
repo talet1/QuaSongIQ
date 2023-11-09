50f080f53e8b37d81213b8313f64b35af3804301
Talet Pytago <90024069+talet1@users.noreply.github.com>
2023-11-09 09:55:31 +0700

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-    cout << "Side 1: " << string(numPriests[1], 'P') << " " << string(numDevils[1], 'D') << endl;
-        cout << "       " << (boatSide == 1 ? "Boat: " : "     ") << string(numPriests[boatSide], 'P') << " " << string(numDevils[boatSide], 'D') << endl;
-        cout << "~~~~~~~~~~~RIVER~~~~~~~~~~~" << endl;
-        cout << "       " << (boatSide == 2 ? "Boat: " : "     ") << string(numPriests[3 - boatSide], 'P') << " " << string(numDevils[3 - boatSide], 'D') << endl;
-        cout << "Side 2: " << string(numPriests[2], 'P') << " " << string(numDevils[2], 'D') << endl;
